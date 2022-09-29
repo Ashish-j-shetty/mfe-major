9c54237f8a5bd98ed64e85a093c2ead38b16f446
Ashish J shetty <ashish.s@goodworker.in>
2022-09-29 17:17:14 +0530

    routing updates amongst the MFE

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,12 +1,31 @@
 import React from "react";
 import ReactDom from "react-dom";
-
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 //? mount funtion to start up the app
 
-const mount = (el) => {
-  ReactDom.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDom.render(<App history={history} />, el);
+
+  //? to have some line of communication from container to this application we provide some function that the container app can call.
+  return {
+    onParentNavigate(location) {
+      const { pathname: nextPathname } = location;
+
+      //current path
+      const pathname = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 //? if we are in development and in isolation call mount function immediately
@@ -14,7 +33,8 @@ const mount = (el) => {
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    //? {} to satisfy the 2nd argument.
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
