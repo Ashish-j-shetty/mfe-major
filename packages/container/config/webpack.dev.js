d2a5598adf48e485565ba1dcc83fe040bc0394bc
Ashish J shetty <ashish.s@goodworker.in>
2022-09-29 08:45:54 +0530

    cicd pipeline and prod webpack setup

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,4 +1,3 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
@@ -20,10 +19,6 @@ const devConfig = {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
       shared: packageJson.dependencies,
-    }),
-
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
